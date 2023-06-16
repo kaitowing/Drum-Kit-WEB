933956d17cb0ee752b0fa90377d4f70eb22fd1ed
arthur <pizzolatti.arthur@edu.pucrs.br>
2023-06-16 17:25:26 -0300

    Added sounds and images to buttons

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-
 document.querySelectorAll(".drum").forEach(button => {
-    button.addEventListener("click", handleClick)
+
+    button.addEventListener('click', function () {
+        switch (button.innerHTML) {
+            case 'w':
+                var audio = new Audio("sounds/crash.mp3")
+                audio.play();
+                break;
+            case 'a':
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            case 's':
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+            case 'd':
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case 'j':
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case 'k':
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case 'l':
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+
+        }
+    })
 });
-function handleClick(){
-    alert("Click")
-}
